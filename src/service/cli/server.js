ffed2c2997b6f3c23eb7cac51f57225c78ccedc5
Oleynichenko <oleynichenkos@gmail.com>
2022-04-14 21:03:02 +0300

    заменил http сервер на express в service

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,63 +1,30 @@
 'use strict';
 
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
-const {HttpCode} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
-
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILENAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-
-      break;
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      break;
-  }
-};
 
 module.exports = {
   name: `--server`,
   run(value) {
     const port = Number.parseInt(value, 10) || DEFAULT_PORT;
+    const app = express();
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, () => {
-        console.info(chalk.green(`Ожидаю соединений на ${port}`));
-      })
-      .on(`error`, ({message}) => {
-        console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
-      });
+    app.use(express.json());
+
+    app.get(`/posts`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (_err) {
+        res.send([]);
+      }
+    });
+
+    app.listen(DEFAULT_PORT, () => console.log(chalk.green(`Сервер запущен на порту ${port}`)));
   }
 };
